ca37913e75601bffe2a9252924429a80005806d7
hollyr <hollyr@a08e899b-0808-4ca7-b456-f970955fd68b>
2010-12-27 09:19:54 +0000

    Doxygen finetuning

--- src/MetricsBuilder.cpp
+++ src/MetricsBuilder.cpp
@@ -16,6 +16,10 @@
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 ///////////////////////////////////////////////////////////////////////////////
 
+/** MetricsBuilder -- implementation file.
+ * @file
+ */
+
 #include "MetricsBuilder.h"
 #include "IssueTable.h"
 #include "MetricsReporter.h"
